ce5429cd407e9510e0367e352fa9b3816b79d37b
Ludovic Ortega <mastership@hotmail.fr>
2019-02-03 21:03:20 +0100

    Fix linux

--- Client/functions/test.py
+++ Client/functions/test.py
@@ -315,9 +315,9 @@ def start_speedtest(type, address, port, arg):
     for key in options:
         try:
             if os.name == "nt":
-                proc = subprocess.Popen("iperf3" + " -c " + address + " -p " + port + " -J " + options[key], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                proc = subprocess.Popen(("iperf3" + " -c " + address + " -p " + port + " -J " + options[key]), shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             else:
-                proc = subprocess.Popen(["iperf3"] + [" -c " + address + " -p " + port + " -J " + options[key]], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                proc = subprocess.Popen(["iperf3", " -c ", address, " -p ", port, " -J ", options[key]], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             result = json.loads(proc.communicate()[0].decode())
             if "error" not in result:
 
